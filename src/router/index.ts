0455ad44ed033eba9944b2f89a71ad63c5121cb9
solakmirnesdev <solakmirnes.dev@gmail.com>
2022-08-31 04:02:37 +0200

    styling

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "../views/home/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import BlocksView from "../views/blocks/BlocksView.vue";
 import BlockView from "../views/blocks/BlockView.vue";
