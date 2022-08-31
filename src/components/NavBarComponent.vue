2ad8c98c2171429fc82b461167e44171f661d1d6
solakmirnes.dev <solakmirnes.dev@gmail.com>
2022-08-31 19:37:48 +0200

    Vue component management and structure

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,57 +1,34 @@
-<script setup>
-
+<script>
+export default {
+  name: 'NavbarComponent'
+}
 </script>
 
 <template>
     <nav class="navbar-expand-lg">
         <div class="flex justify-around items-center">
-            <img src="../assets/Vector.svg" alt="logo">
+            <img src="../assets/nav-logo.svg" alt="logo">
             <div class="navbar-nav text-white" style="margin: 0 1em">
                 <RouterLink to="/">HOME</RouterLink>
-                <RouterLink to="/about">BLOCKS & TXS</RouterLink>
-                <RouterLink to="/epochs">CHECKPOINTS</RouterLink>
-                <RouterLink to="/epochs">VALIDATORS</RouterLink>
+                <RouterLink to="/blocks-and-txs">BLOCKS & TXS</RouterLink>
+                <RouterLink to="/checkpoints">CHECKPOINTS</RouterLink>
+                <RouterLink to="/validators">VALIDATORS</RouterLink>
             </div>
         </div>
     </nav>
 </template>
 
 <style scoped>
+
 nav {
     padding: 0;
     margin: 0;
     border-bottom: 1px solid rgba(255, 255, 255, 0.2);
 }
 
-
 .active-link {
   border-bottom: 3px solid #23A5DC;
 }
-
-.navbar-brand {
-    color: white;
-    font-size: 2rem;
-    font-weight: 900;
-}
-
-
-.navbar-content {
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    margin: 0 auto;
-    max-width: 1280px;
-}
-
-/*.navbar-nav a {*/
-/*    display: inline-block;*/
-/*    padding: 0.4em 1rem;*/
-/*    color: white;*/
-/*    text-decoration: none;*/
-/*    width: 100px;*/
-/*    border-radius: 5px;*/
-/*    text-align: center;*/
-/*}*/
 
 a {
   padding: 40px 0;
@@ -62,6 +39,7 @@ a {
   letter-spacing: 3px;
   text-align: right;
 }
+
 </style>
 
 
---
+++ src/views/checkpoints/CheckpointsView.vue
@@ -0,0 +1,12 @@
+<script>
+
+export default {
+    name: 'CheckpointsView'
+}
+</script>
+
+<template>
+    <div>
+        <h1>Checkpoints</h1>
+    </div>
+</template>
