6369220540525dc3937b887bd1ef80d89dfe8e78
endlessorhub <anthonyhutchi99@gmail.com>
2022-09-02 08:43:02 +0200

    componentizing

---
+++ src/views/home/BtcBlockItem.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+export default {
+    name: 'BtcBlockItem',
+    props: {
+        hasLogo: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="block-content items-center absolute">
+        <img class="item" src="../../assets/btc-logo.svg" alt="item" />
+    </div>
+</template>
+
+<style scoped>
+
+.block-content {
+    text-align: center;;
+}
+
+.item {
+    position: relative;
+    top: 20px;
+    left: 22px;
+    width: 39px;
+    height: 51px;
+}
+
+.disabled {
+    background: #F7931A;
+    opacity: 0.3;
+}
+
+</style>
