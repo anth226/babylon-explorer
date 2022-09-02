73dcaae661acfed65710fa4d4c4cd9f4593490e1
solakmirnes.dev <solakmirnes.dev@gmail.com>
2022-09-02 10:01:53 +0200

    arrows

--- src/views/home/BtcBlockItem.vue
+++ src/views/home/BtcBlockItem.vue
@@ -5,15 +5,30 @@ export default {
         hasLogo: {
             type: Boolean,
             default: false
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false
+        },
+        disable: {
+            type: Boolean
         }
-    }
+    },
 }
 </script>
 
 <template>
-    <div class="block-item">
-        <div class="block-content items-center absolute">
+    <div :class="isDisabled ? 'disabled' : ''" class="block-item">
+            <div v-if="hasLogo" class="block-content items-center absolute">
             <img class="item" src="../../assets/btc-logo.svg" alt="item" />
+        </div>
+
+        <div class="absolute">
+            <div class="capsul">
+                <img class="capsul-arrow" src="../../assets/capsul-arrow.svg" />
+                <img class="capsul-line" src="../../assets/capsul-line.svg" />
+                <img class="capsul-circle" src="../../assets/capsul-circle.svg" />
+            </div>
         </div>
     </div>
 </template>
@@ -24,6 +39,21 @@ export default {
     text-align: center;;
 }
 
+.capsul {
+    border-radius: 20px;
+    display: flex;
+    width: 35px;
+    background: white;
+    position: absolute;
+    top: 36px;
+    right: -6px;
+}
+
+.capsul-line {
+    width: 9px;
+}
+
+
 .item {
     position: relative;
     top: 20px;
@@ -32,16 +62,21 @@ export default {
     height: 51px;
 }
 
-.block-item {
+.block-item  {
     width: 90px;
     height: 91px;
     background: #F7931A;
     border-radius: 20px;
+    cursor: pointer;
+}
+
+.block-item:hover {
+    scale: 1.03;
+    transition-duration: 200ms;
 }
 
 .disabled {
-    background: #F7931A;
-    opacity: 0.3;
+    background: rgba(247, 147, 26, 0.3);
 }
 
 </style>
