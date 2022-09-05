b672a4e812fda648c0baaee5c2ac54b540c95f7e
endlessorhub <anthonyhutchi99@gmail.com>
2022-09-06 01:37:59 +0200

    componentizing

--- src/views/home/EpochItem.vue
+++ src/views/home/EpochItem.vue
@@ -1,31 +1,82 @@
 <script lang="ts">
+import EpochSmallBlock from './EpochSmallBlock.vue'
 
 export default {
-    name: 'EpochItem'
+    name: 'EpochItem',
+    components: {
+        EpochSmallBlock
+    },
+    props: {
+        hasArrow: {
+            type: Boolean,
+            default: false
+        }
+    }
 }
 </script>
 
 <template>
-    <div class="small-block"></div>
-    <div class="small-block"></div>
-        <div class="mx-3">
-            ...
+    <div class="main-section">
+        <div class="flex items-center">
+            <EpochSmallBlock />
+            <div class="small-block"></div>
+            <div class="mx-3">
+                ...
+            </div>
+            <div class="small-block"></div>
+            <div class="small-block"></div>
         </div>
-    <div class="small-block"></div>
-    <div class="small-block"></div>
+        <div class="blank w-full bg-red-400"></div>
+        <div class="title-section w-full mt-3">
+            <div class="heading">
+                Epoch
+            </div>
+            <div class="subheading">
+                (100 blocks)
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
 
-    .block {
-        display: block !important;
-    }
+.title-section {
+    text-align: center;
+}
 
-    .small-block {
-        width: 40px;
-        height: 40px;
-        background: var(--primary-blue);
-        border-radius: 10px;
-        margin: 0 10px;
-    }
+.heading {
+    font-weight: 600;
+    font-size: 18px;
+}
+
+.subheading {
+    font-weight: 300;
+    font-size: 12px;
+}
+
+.blank {
+    height: 10px;
+    background: var(--primary-blue);
+    opacity: 0.1;
+    border-radius: 15px;
+    position: relative;
+    top: 9px;
+}
+
+
+.main-section {
+    align-items: center;
+    position: relative;
+    top: 27px;
+    margin: 0 20px;
+}
+
+.small-block {
+    width: 40px;
+    height: 40px;
+    background: rgba(1, 12, 194, 0.4);
+    border-radius: 10px;
+    margin: 0 15px
+}
+
 </style>
---
+++ src/views/home/EpochSmallBlock.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+
+export default {
+    name: 'EpochSmallBlock'
+}
+</script>
+
+<template>
+    <div class="small-block">
+        <div v-if="hasArrow" class="absolute">
+            <div class="capsul">
+                <img class="capsul-arrow" src="../../assets/capsul-arrow.svg" />
+                <img class="capsul-line" src="../../assets/capsul-line.svg" />
+                <img class="capsul-circle" src="../../assets/capsul-circle.svg" />
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+.capsul {
+    border-radius: 20px;
+    display: flex;
+    width: 35px;
+    background: white;
+    position: absolute;
+    top: 13px;
+    right: -6px;
+}
+
+.capsul-line {
+    width: 9px;
+}
+
+.small-block {
+    width: 40px;
+    height: 40px;
+    background: rgba(1, 12, 194, 0.4);
+    border-radius: 10px;
+    margin: 0 15px
+}
+
+</style>
