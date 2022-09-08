c5832ee1b1a4b8ffc37cdc605b58d19a8bd9fdd3
endlessorhub <anthonyhutchi99@gmail.com>
2022-09-08 04:01:43 +0200

    Adding data

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,22 +3,7 @@ const routes = [
         path: "/",
         name: "home",
         component: () => import(/* webpackChunkName: "HomeView" */ '../views/home/HomeView.vue')
-    },
-    {
-        path: "/blocks-and-txs",
-        name: "blocks-and-txs",
-        component: () => import(/* webpackChunkName: "AboutView */ '../views/blocks-and-txs/BlocksAndTXS.vue')
-    },
-    {
-        path: "/checkpoints",
-        name: "checkpoints",
-        component: () => import(/* webpackChunkName: "CheckpointsView */ '../views/checkpoints/CheckpointsView.vue')
-    },
-    {
-        path: '/validators',
-        name: 'Validators',
-        component: () => import(/* webpackChunkName: "ValidatorsView" */ '../views/validators/ValidatorsView.vue')
-    },
+    }
 ]
 
 export default routes
