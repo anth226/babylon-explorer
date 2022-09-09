a586ffb276862411390d3889948a75e728259913
endlessorhub <anthonyhutchi99@gmail.com>
2022-09-09 14:09:53 +0200

    resolving merge issues

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,37 +8,7 @@ const routes = [
         path: '/block/:blockID',
         name: 'block',
         component: () => import(/* webpackChunkName: "BlocksView" */ '../views/blocks/BlockView.vue')
-    },
-    {
-        path: "/about",
-        name: "about",
-        component: AboutView,
-    },
-    {
-        path: "/blocks",
-        name: "blocks",
-        component: BlocksView,
-    },
-    {
-        path: "/blocks/:blockID",
-        name: "specificBlock",
-        component: BlockView,
-    },
-    {
-        path: "/epochs",
-        name: "epochs",
-        component: EpochingListView,
-    },
-    {
-        path: "/epochs/:epochNum",
-        name: "specificEpoch",
-        component: EpochDetailViewVue,
-    },
-    {
-        path: "/metrics",
-        name: "metrics",
-        component: MetricsListViewVue,
-    },
+    }
 ]
 
 export default routes
