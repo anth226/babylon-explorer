29cee5fb53f50ed25bdbb5346e26ba5c59ea55c0
solakmirnes.dev <solakmirnes.dev@gmail.com>
2022-09-11 19:26:24 +0200

    routing fix

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,13 @@ export default {
 
 <template>
     <div id="app">
-        <NavBarComponent extended />
+        <NavBarComponent />
         <transition
             mode="out-in"
             enter-active-class="animate__animated animate__fadeIn"
             leave-active-class="animate__animated animate__fadeOut"
         >
-            <RouterView extended />
+            <RouterView />
         </transition>
     </div>
 </template>
