948c3239a3ec151b031c6731ef6a47354910ce0a
solakmirnes.dev <solakmirnes.dev@gmail.com>
2022-09-11 19:39:37 +0200

    some small ts refactor

--- src/shims-vuex.d.ts
+++ src/shims-vuex.d.ts
@@ -1,12 +1,7 @@
 import { Store } from 'vuex'
 
 declare module '@vue/runtime-core' {
-  // Declare your own store states.
   interface State {
     count: number
-  }
-
-  interface ComponentCustomProperties {
-    $store: Store<State>
   }
 }
